2c335f3b578234dbc8efe2a50a17cd8827d6f7f5
Suvid-Singhal <30971624+Suvid-Singhal@users.noreply.github.com>
2020-01-02 14:48:34 +0530

    Update data.py

--- data.py
+++ data.py
@@ -5,10 +5,10 @@ def News():
     today = date.today()
     tendaysago = today - timedelta(days=5)
 
-    all_articles = get("https://newsapi.org/v2/everything?qInTitle=Open-Source&apiKey=3cf8688e71e4497d9f9cfd0684457b15").json()
+    all_articles = get("https://newsapi.org/v2/everything?qInTitle=Open-Source&apiKey=<YOUR-API-KEY-HERE>").json()
     desired_articles = all_articles.get("articles")
-    all_articles = get("https://newsapi.org/v2/everything?qInTitle=Linux&apiKey=3cf8688e71e4497d9f9cfd0684457b15").json()
+    all_articles = get("https://newsapi.org/v2/everything?qInTitle=Linux&apiKey=<YOUR-API-KEY-HERE>").json()
     desired_articles += all_articles.get("articles")
-    all_articles = get("https://newsapi.org/v2/everything?qInTitle=Android&apiKey=3cf8688e71e4497d9f9cfd0684457b15").json()
+    all_articles = get("https://newsapi.org/v2/everything?qInTitle=Android&apiKey=<YOUR-API-KEY-HERE>").json()
     desired_articles += all_articles.get("articles")
     return desired_articles
