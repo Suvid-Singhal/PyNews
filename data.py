752ed11e92d937e4079821e8f926e2187f177405
Suvid Singhal <30971624+Suvid-Singhal@users.noreply.github.com>
2021-09-30 10:57:53 +0530

    Update data.py

--- data.py
+++ data.py
@@ -1,9 +1,9 @@
 def News(keywords):
     from requests import get
     import json
-    all_articles = get("https://newsapi.org/v2/everything?qInTitle="+keywords[0]+"&apiKey=3cf8688e71e4497d9f9cfd0684457b15").json()
+    all_articles = get("https://newsapi.org/v2/everything?qInTitle="+keywords[0]+"&apiKey=<YOUR-KEY-HERE>").json()
     desired_articles = all_articles.get("articles")
     for i in range(1,len(keywords)):
-        all_articles = get("https://newsapi.org/v2/everything?qInTitle="+keywords[i]+"&apiKey=3cf8688e71e4497d9f9cfd0684457b15").json()
+        all_articles = get("https://newsapi.org/v2/everything?qInTitle="+keywords[i]+"&apiKey=<YOUR-KEY-HERE>").json()
         desired_articles += all_articles.get("articles")
     return desired_articles
