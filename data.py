c22c93066299aba1eab6776986cba7726d6493fa
Suvid Singhal <30971624+Suvid-Singhal@users.noreply.github.com>
2023-07-10 23:33:44 +0530

    Update data.py
    Enable the code to access API key stored in an environment variable

--- data.py
+++ data.py
@@ -1,9 +1,11 @@
+import os
+api_key=os.getenv('key')
 def News(keywords):
     from requests import get
     import json
-    all_articles = get("https://newsapi.org/v2/everything?qInTitle="+keywords[0]+"&apiKey=<YOUR-KEY-HERE>").json()
+    all_articles = get("https://newsapi.org/v2/everything?qInTitle="+keywords[0]+"&apiKey="+api_key).json()
     desired_articles = all_articles.get("articles")
     for i in range(1,len(keywords)):
-        all_articles = get("https://newsapi.org/v2/everything?qInTitle="+keywords[i]+"&apiKey=<YOUR-KEY-HERE>").json()
+        all_articles = get("https://newsapi.org/v2/everything?qInTitle="+keywords[i]+"&apiKey="+api_key).json()
         desired_articles += all_articles.get("articles")
     return desired_articles
